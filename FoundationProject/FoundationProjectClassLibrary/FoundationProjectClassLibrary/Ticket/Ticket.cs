89b46d07af8386480c1955f169b43e18484fc0b5
jaku14 <jameskuhn91@gmail.com>
2022-06-28 19:59:08 -0400

    models created

---
+++ FoundationProject/FoundationProjectClassLibrary/FoundationProjectClassLibrary/Ticket/Ticket.cs
@@ -0,0 +1,29 @@
+﻿namespace Models;
+
+public class Ticket
+{
+
+    public string ID {get; set;}
+    public string author {get; set;}
+    public string resolver {get; set;}
+    public string description {get; set;}
+    public string status {get; set;}
+    public double amount {get; set;}
+
+
+}
+
+
+public ticket(String ID, string author, string resolver, string description, string status, double amount)
+{
+    this.ID = ID;
+    this.author = author;
+    this.resolver = resolver;
+    this.description = description;
+    this.status = status;
+    this.amount = amount;
+
+
+
+
+}
